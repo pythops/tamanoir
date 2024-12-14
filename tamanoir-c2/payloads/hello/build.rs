ba9084cd22326114984631578f88f10f0fcb5f2b
adrien gaultier <adrien.gaultier@protonmail.com>
2024-12-14 20:28:48 +0100

    fix

--- tamanoir-c2/payloads/hello/build.rs
+++ tamanoir-c2/payloads/hello/build.rs
@@ -11,14 +11,8 @@ fn main() {
         target_arch, binary_name
     );
     let bin_path = format!("target/release/{}.bin", binary_name);
-    // let output = Command::new("objcopy")
-    //     .arg("-O")
-    //     .arg("binary")
-    //     .arg(&elf_path)
-    //     .arg(&bin_path)
-    //     .output()
-    //     .expect("Failed to run objcopy");
-    let output = Command::new("strip")
+
+    let output = Command::new("x86_64-linux-gnu-strip")
         .arg("-s")
         .arg("--strip-unneeded")
         .arg(&elf_path)
@@ -27,9 +21,23 @@ fn main() {
 
     if !output.status.success() {
         panic!(
-            "objcopy failed with status {}: {}",
+            "strip failed with status {}: {}",
             output.status,
             String::from_utf8_lossy(&output.stderr)
         );
+    }
+    let output = Command::new("x86_64-linux-gnu-objcopy")
+        .arg("-O")
+        .arg("binary")
+        .arg(&elf_path)
+        .arg(&bin_path)
+        .output()
+        .expect("Failed to run objcopy");
+    if !output.status.success() {
+        panic!(
+            "objcopy failed with status {}: {}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr)
+        )
     }
 }
