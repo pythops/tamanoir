0d37b4ecfc50f0971e602d106a2e3a00cea8318d
adrien gaultier <adrien.gaultier@protonmail.com>
2024-12-14 21:39:42 +0100

    fix

--- tamanoir-c2/x_build_utils/build.rs
+++ tamanoir-c2/x_build_utils/build.rs
@@ -1,39 +1,41 @@
 use std::process::Command;
 
-fn main() {
-    #[cfg(target_arch = "x86_64")]
-    {
-        let binary_name = std::env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME not set");
-        let elf_path = format!("target/x86_64-unknown-linux-gnu/release/{}", binary_name);
-        let bin_path = format!("target/release/{}_x86_64.bin", binary_name);
+#[cfg(target_arch = "x86_64")]
+fn build() {
+    let binary_name = std::env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME not set");
+    let elf_path = format!("target/x86_64-unknown-linux-gnu/release/{}", binary_name);
+    let bin_path = format!("target/release/{}_x86_64.bin", binary_name);
 
-        let output = Command::new("x86_64-linux-gnu-strip")
-            .arg("-s")
-            .arg("--strip-unneeded")
-            .arg(&elf_path)
-            .output()
-            .expect("Failed to run strip");
+    let output = Command::new("x86_64-linux-gnu-strip")
+        .arg("-s")
+        .arg("--strip-unneeded")
+        .arg(&elf_path)
+        .output()
+        .expect("Failed to run strip");
 
-        if !output.status.success() {
-            panic!(
-                "strip failed with status {}: {}",
-                output.status,
-                String::from_utf8_lossy(&output.stderr)
-            );
-        }
-        let output = Command::new("x86_64-linux-gnu-objcopy")
-            .arg("-O")
-            .arg("binary")
-            .arg(&elf_path)
-            .arg(&bin_path)
-            .output()
-            .expect("Failed to run objcopy");
-        if !output.status.success() {
-            panic!(
-                "objcopy failed with status {}: {}",
-                output.status,
-                String::from_utf8_lossy(&output.stderr)
-            )
-        }
+    if !output.status.success() {
+        panic!(
+            "strip failed with status {}: {}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr)
+        );
     }
+    let output = Command::new("x86_64-linux-gnu-objcopy")
+        .arg("-O")
+        .arg("binary")
+        .arg(&elf_path)
+        .arg(&bin_path)
+        .output()
+        .expect("Failed to run objcopy");
+    if !output.status.success() {
+        panic!(
+            "objcopy failed with status {}: {}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr)
+        )
+    }
+}
+
+fn main() {
+    build()
 }
